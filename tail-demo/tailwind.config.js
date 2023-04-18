9838bf5f48b09e05ad8f623e7ff795c2a6fec059
Nickzsche <1815101044@isparta.edu.tr>
2023-04-18 17:25:15 +0300

    Header

--- tail-demo/tailwind.config.js
+++ tail-demo/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      }
+    },
+    extend: {
+      fontFamily: {
+        open_sans: ["Open Sans", "sans-serif"],
+        gemunus: ["Gemunu Libre", "sans-serif"],
+      },
+      colors: {
+        "gaga-red": "#BC1A45",
+        "gaga-melon": "#FFD369",
+        "gaga-grey":"#DDDDDD",
+        "gaga-white":"#F7F7F7"
+      }
+    },
   },
   plugins: [],
 }
